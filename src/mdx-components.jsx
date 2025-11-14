c88d771056009e5f41f4b02625c1b8cc9549e368
Nate Bass <nate.bass@outlook.com>
2025-11-13 19:17:50 -0800

    Clean up project configuration files.
    * Remove SASS.
    * Move the base path utility to the GitHub Pages integration folder. Refactor the file and documentation.
    * Use the caret ^ semantic versioning for all packages and upgrade packages.

--- src/mdx-components.jsx
+++ src/mdx-components.jsx
@@ -1,14 +1,9 @@
 const components = {};
 
 /**
- * This file is required.
+ * This file is required to use @next/mdx with the App Router and will not work without it.
  *
  * @see https://nextjs.org/docs/app/guides/mdx
- *
- * ### Good to know:
- * - mdx-components.tsx is required to use @next/mdx with App Router and will not work without it.
- * - Learn more about the mdx-components.tsx file convention.
- * - Learn how to use custom styles and components.
  */
 export function useMDXComponents() {
   return components;
