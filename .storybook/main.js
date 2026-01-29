def743fcfa964b8d9079e19a27770674e85d0b96
Nate Bass <nate.bass@outlook.com>
2026-01-29 02:22:50 -0800

    ammend - Fix the Storybook image basepath on GitHub Pages.

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,17 +3,8 @@ import getBasePath from "../src/integrations/gh-pages/getBasePath.mjs";
 
 const basePath = getBasePath();
 
-if (typeof window !== "undefined") {
-  window.__NEXT_DATA__ = {
-    basePath: basePath,
-  };
-}
-
 /**
  * The main storybook config file.
- * Inject the basePath into Storybook's environment variables and tell Vite to use the base path for assets.
- * Mock the Next.js internal state so <Image /> knows the prefix.
- * Ensure it looks like "/repo-name/" not "/repo-name".
  */
 export default defineMain({
   framework: "@storybook/nextjs-vite",
